746ba0b66496ebf2f8aed03a5930139700fe5b8e
monopoly11141 <monopoly1114@vt.edu>
2023-10-20 13:39:07 +0900

    ~Lesson 84

--- app/src/main/java/com/example/udemy_paulo_introtocompose/MainActivity.kt
+++ app/src/main/java/com/example/udemy_paulo_introtocompose/MainActivity.kt
@@ -3,14 +3,11 @@ package com.example.udemy_paulo_introtocompose
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -32,27 +29,56 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyApp() {
+
+    var moneyCounter by remember { mutableStateOf(0) }
+
     Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-        Text(text = "hello")
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                text = "$${moneyCounter * 100}",
+                fontSize = 24.sp
+            )
+
+            Spacer(
+                modifier = Modifier
+                    .height(30.dp)
+            )
+
+            CreateCircle(moneyCounter) {
+                moneyCounter = it + 1
+            }
+        }
     }
 }
 
 @OptIn(ExperimentalMaterial3Api::class)
-@Preview
 @Composable
-fun CreateCircle() {
+fun CreateCircle(moneyCounter: Int = 0, updateMoneyCounter: (Int) -> Unit) {
+
     Card(
         modifier = Modifier
             .padding(3.dp)
-            .size(50.dp),
-        shape = CircleShape
-    ){
+            .size(100.dp)
+            .clickable {
+                updateMoneyCounter(moneyCounter)
+                //moneyCounter += 1
+            },
+        shape = CircleShape,
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 4.dp
+        )
+    ) {
         Box(
+            Modifier
+                .fillMaxWidth()
+                .fillMaxHeight(),
             contentAlignment = Alignment.Center
         ) {
             Text(
                 text = "Tap",
-                modifier = Modifier
             )
         }
 
